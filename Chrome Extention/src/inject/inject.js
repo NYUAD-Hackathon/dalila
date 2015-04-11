0a02ea4efcd30004cb65759fcb0e0bdfb3a216c6
Ayman Awartani <ayman@harri.com>
2015-04-11 17:02:14 +0400

    add dynamic popover

--- Chrome Extention/src/inject/inject.js
+++ Chrome Extention/src/inject/inject.js
@@ -12,42 +12,11 @@ chrome.extension.sendMessage({}, function(response) {
 	}, 10);
 });
 
-    function result(word, callback) {
-    	callback({
-    		prefix: {
-    			name: 'wmbt',
-    			category: 'Pref-IV-yu-ELSE',
-    			diac: 'wimA_bitu',
-    			bw: 'wi/PART/NEG_PART/PROG_PART/IV2MS',
-    			gloss: 'so/and_+_not_+_[present-tense]_+_you_',
-    			pos: 'verb'
-    		},
-    		suffix: {
-    			name: 'km',
-    			category: 'Suff-NOM-stem-V-POSS',
-    			diac: 'kum',
-    			bw: 'kum/POSS_PRON_2P',
-    			gloss: 'your_'
-
-    		},
-    		stem: {
-    			name: 'brA',
-    			category: 'N',
-    			diac: 'barA',
-    			lex:'bariy_1',
-    			bw:'barA/ADJ',
-    			gloss:'innocent;exempt_',
-    			pos:'adj'
-    		}
-
-    });
-
-};
-
-
-
-
 $(document).ready(function() {
+
+    var popover = $("<div>").addClass('dalila_popover').hide();
+    $('body').append(popover);
+
 
     var p = $('p');
     p.css({ cursor: 'pointer' });
@@ -56,10 +25,51 @@ $(document).ready(function() {
         var range = window.getSelection() || document.getSelection() || document.selection.createRange();
         var word = $.trim(range.toString());
         if(word != '') {
-            alert(word);
+            showPopover(word, event.pageX, event.pageY);
         }
         range.collapse();
         e.stopPropagation();
     });
 
 });
+
+function showPopover(word, x, y) {
+    dalilaProcess(word, function(result) {
+        $('.dalila_popover').show().html(result.stem.gloss).css({top:y, left:x})
+    });
+}
+
+
+
+function dalilaProcess(word, callback) {
+
+    var result = {
+        prefix: {
+            name: 'wmbt',
+            category: 'Pref-IV-yu-ELSE',
+            diac: 'wimA_bitu',
+            bw: 'wi/PART/NEG_PART/PROG_PART/IV2MS',
+            gloss: 'so/and_+_not_+_[present-tense]_+_you_',
+            pos: 'verb'
+        },
+        suffix: {
+            name: 'km',
+            category: 'Suff-NOM-stem-V-POSS',
+            diac: 'kum',
+            bw: 'kum/POSS_PRON_2P',
+            gloss: 'your_'
+
+        },
+        stem: {
+            name: 'brA',
+            category: 'N',
+            diac: 'barA',
+            lex:'bariy_1',
+            bw:'barA/ADJ',
+            gloss:'innocent;exempt_',
+            pos:'adj'
+        }
+    };
+
+    callback(result);
+};
