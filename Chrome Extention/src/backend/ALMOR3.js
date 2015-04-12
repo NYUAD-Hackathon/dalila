18142c02ae6723aa907ec3baee1d7a3e9fea4fad
Maeda Hanafi <my_pink_han@yahoo.com>
2015-04-12 06:38:43 +0400

    armor code indented

--- Chrome Extention/src/backend/ALMOR3.js
+++ Chrome Extention/src/backend/ALMOR3.js
@@ -1,15 +1,12 @@
-//var hash_stem = require('./data.json');
-//var hash_prefix = require('./data.json');
 
-//analyzeSolutions("wsyktbwnhA");
 
 
 function analyzeSolutions(word){
 
-	word = toBuckwalter(word)
-	var output = {}; 										//var (@output) = ();
-    var solutions = []; 									//var @solutions = ();
-    var cnt = 0; 											//var $cnt = 0;
+	word           = toBuckwalter(word)
+	var output     = {}; 									//var (@output) = ();
+    var solutions  = []; 									//var @solutions = ();
+    var cnt        = 0; 									//var $cnt = 0;
 
 
     //// Handle deterministic (trivial) cases
@@ -22,8 +19,8 @@ function analyzeSolutions(word){
 		//var hash_res 		= hash["DEFAULT pos:noun"]		//$$hash{"DEFAULT pos:noun"}
 		var default_obj 	= {}//hash_res; 				//var %DEFAULT=%{$$hash{"DEFAULT pos:noun"}};
 		default_obj.diac 	= word; 						//$DEFAULT{"diac"}=$word;
-		default_obj.lex 	= word;// + "_0";					//$DEFAULT{"lex"}=$word."_0";
-		default_obj.bw 		= "FOREIGN";			//$DEFAULT{"bw"}=$word."/FOREIGN";
+		default_obj.lex 	= word;// + "_0";				//$DEFAULT{"lex"}=$word."_0";
+		default_obj.bw 		= "FOREIGN";			        //$DEFAULT{"bw"}=$word."/FOREIGN";
 		default_obj.gloss	= word; 						//$DEFAULT{"gloss"}=$word;
 		default_obj.source 	= "foreign";					//$DEFAULT{"source"}="foreign";
 		return [default_obj];								//return [$analysis];
@@ -65,122 +62,96 @@ function analyzeSolutions(word){
     var max_prob = -1000000;
     var segmented = segmentword(matchword);							//var @segmented= @{ &segmentword($matchword,$hash) };
     for(var t=0; t<segmented.length; t++){							//foreach var $segmentation (@segmented) {
-	 	var segmentation 	= segmented[t];						//#print "( $segmentation )\n";
+	 	var segmentation 	= segmented[t];						    //#print "( $segmentation )\n";
 
-		var prefix 			= segmentation[0];							//var ($prefix,$stem,$suffix) = split ("\t",$segmentation);
+		var prefix 			= segmentation[0];						//var ($prefix,$stem,$suffix) = split ("\t",$segmentation);
 		var stem 			= segmentation[1];
 		var suffix 			= segmentation[2];
 
 
 
 	    console.log(prefix + " " + stem + " " + suffix)					//#  print "($prefix,$stem,$suffix)\n";
-
         var stem_value_list = hash_stem[stem];
 
 	    for (var i in stem_value_list) {								//for(var u=0; u<hash_stem.length; u++){							//foreach var $stem_value (@{$$hash{"stem#$stem"}}){
-            var stem_value = stem_value_list[i]
-			//var stem_value	= hash_stem[u];
-			var cat_b 		= stem_value["category"]					//var  $cat_b = $$stem_value{"XAMACAT"};
-
+            var stem_value        = stem_value_list[i]
+			var cat_b 		      = stem_value["category"]				//var  $cat_b = $$stem_value{"XAMACAT"};
             var prefix_value_list = hash_prefix[prefix];
 
-			for (var j in prefix_value_list) {						//for(var v=0; v<hash_prefix.length; v++){					//foreach var $prefix_value (@{$$hash{"pre#$prefix"}}) {
-                var prefix_value = prefix_value_list[j];
-
-		    	//var prefix_value 	= hash_prefix[v];
+			for (var j in prefix_value_list) {						    //for(var v=0; v<hash_prefix.length; v++){					//foreach var $prefix_value (@{$$hash{"pre#$prefix"}}) {
+                var prefix_value    = prefix_value_list[j];
 		    	var cat_a 			= prefix_value["category"] 			//var $cat_a = $$prefix_value{"XAMACAT"};
 
-		    	if( table.table_AB[cat_a+"!"+cat_b]  ){ 				//if ( exists($$hash{"tAB#$cat_a $cat_b"}) ) {
-
+		    	if( table.table_AB[cat_a + "!" + cat_b]  ){ 			//if ( exists($$hash{"tAB#$cat_a $cat_b"}) ) {
                     var suffix_value_list = hash_suffix[suffix];
-					for (var k in suffix_value_list) {				//for(var w=0; w<hash_suffix.length; w++){			//foreach var $suffix_value (@{$$hash{"suf#$suffix"}}) {
-                        var suffix_value = suffix_value_list[k];
-						//var suffix_value 	= hash_suffix[w];
-
+					
+                    for (var k in suffix_value_list) {				    //for(var w=0; w<hash_suffix.length; w++){			//foreach var $suffix_value (@{$$hash{"suf#$suffix"}}) {
+                        var suffix_value    = suffix_value_list[k];
 						var cat_c 			= suffix_value["category"];	//var $cat_c = $$suffix_value{"XAMACAT"};
 
-			    		if( table.table_BC[cat_b+"!"+cat_c]  ){
-                        								//if ( exists($$hash{"tBC#$cat_b $cat_c"}) ) {
-							if(  table.table_AC[cat_a+"!"+cat_c] ){										//if ( exists($$hash{"tAC#$cat_a $cat_c"}) ) {
+			    		if( table.table_BC[cat_b + "!" + cat_c]  ){
+                        								                //if ( exists($$hash{"tBC#$cat_b $cat_c"}) ) {
+							if(  table.table_AC[cat_a + "!" + cat_c] ){										       //if ( exists($$hash{"tAC#$cat_a $cat_c"}) ) {
 
-				    			var voc_str 	= prefix_value.diac + stem_value.diac + suffix_value.diac;	//var $voc_str = $$prefix_value{"diac"}."+".$$stem_value{"diac"}."+".$$suffix_value{"diac"};
-                                voc_str = replaceAll('_',' ', voc_str);
-                                voc_str = replaceAll('undefined','', voc_str);
+				    			var voc_str = prefix_value.diac + stem_value.diac + suffix_value.diac;	           //var $voc_str = $$prefix_value{"diac"}."+".$$stem_value{"diac"}."+".$$suffix_value{"diac"};
+                                voc_str     = replaceAll('_',' ', voc_str);
+                                voc_str     = replaceAll('undefined','', voc_str);
                                 
-				    			// TODO convert BWMorphotactics to javascript
-				    			//voc_str		= BWMorphotactics(voc_str)												//$voc_str=&BWMorphotactics($voc_str);
-
-				    			//var $unvoc_str=$voc_str;
-				    			var unvoc_str 	= voc_str.replace(/(\s|[aiuo\~\`FKN\_])/g, "");						//$unvoc_str=~s/(\s|[aiuo\~\`FKN\_])//g;
+				    			var unvoc_str = voc_str.replace(/(\s|[aiuo\~\`FKN\_])/g, "");						//$unvoc_str=~s/(\s|[aiuo\~\`FKN\_])//g;
 
 				    			//IGNORE featureMergePSS
 				    			//#prefer values in this order: STEM+DEFAULT < prefix < suffix
 				    			var analysis 	= {}
 				    			analysis.word 	= toUnicode(unvoc_str)
-
 				    			analysis.diac 	= toUnicode(voc_str)
-				    			analysis.pos 	= stem_value["pos"]														//var %analysis = %{ &featureMergePSS($prefix_value,$stem_value,$suffix_value) };
-                                stem_value["lex"] = stem_value["lex"].replace(/(-[uiao])?\_\d$/, "")
-                                analysis.lex = stem_value["lex"]
-
-                                var bw     = prefix_value.bw + "+"+stem_value.bw + "+"+suffix_value.bw;  //var $voc_str = $$prefix_value{"diac"}."+".$$stem_value{"diac"}."+".$$suffix_value{"diac"};
-                                console.log("original bw:", bw)
+				    			analysis.pos 	= stem_value["pos"]													//var %analysis = %{ &featureMergePSS($prefix_value,$stem_value,$suffix_value) };
                                 
-                                //Replace undefined with empty
-                                bw = replaceAll('undefined','', bw);
+                                stem_value["lex"]   = stem_value["lex"].replace(/(-[uiao])?\_\d$/, "")
+                                analysis.lex        = stem_value["lex"]
 
-                                //Replace pluses in the beginning with empty
-                                bw = replaceAll("^\\++", '', bw)
-                                 //Replace pluses in the ending with empty
-                                bw = replaceAll("\\++$", '', bw)
-                                //Replace double pluses with single pluses
-                                bw = replaceAll('\\++', "+", bw);
-
+                                var bw      = prefix_value.bw + "+"+stem_value.bw + "+"+suffix_value.bw;             //var $voc_str = $$prefix_value{"diac"}."+".$$stem_value{"diac"}."+".$$suffix_value{"diac"};
+                                //console.log("original bw:", bw)
+                                bw          = replaceAll('undefined','', bw);   //Replace undefined with empty
+                                bw          = replaceAll("^\\++", '', bw)       //Replace pluses in the beginning with empty
+                                bw          = replaceAll("\\++$", '', bw)       //Replace pluses in the ending with empty
+                                bw          = replaceAll('\\++', "+", bw);      //Replace double pluses with single pluses
                                 analysis.bw = bw
-                                console.log("bw:",bw)
+                                //console.log("bw:",bw)
 
 
                                 var current_prob = -999999;
 
                                 if( lexModel.hasOwnProperty(analysis.lex) && posModel.hasOwnProperty(analysis.bw)){
                                         current_prob = lexModel[analysis.lex] + posModel[analysis.bw]
-                                        console.log("lexModel[analysis.lex]:", lexModel[analysis.lex])
-                                        console.log("posModel[analysis.bw]:", posModel[analysis.bw])
+                                        //console.log("lexModel[analysis.lex]:", lexModel[analysis.lex])
+                                        //console.log("posModel[analysis.bw]:", posModel[analysis.bw])
                                 }else if(analysis.bw.match(/NOUN\_PROP/) != null){
                                     current_prob = -200
                                 }
 
 
 
-                                console.log("analysis.lex:", analysis.lex)
+                                /*console.log("analysis.lex:", analysis.lex)
                                 console.log("analysis.bw:", analysis.bw)
-                                console.log("current_prob:",current_prob)
+                                console.log("current_prob:",current_prob)*/
 
+                                analysis.prob       = current_prob; 
+				    			analysis.lex 	    = toUnicode(analysis.lex)														    //var %analysis = %{ &featureMergePSS($prefix_value,$stem_value,$suffix_value) };
+				   				analysis.lexgloss   = stem_value["gloss"]																//# $analysis{"gloss"} = $$stem_value{"gloss"};
+							    analysis.gloss 	    = prefix_value["gloss"] + " "+ stem_value["gloss"] + " " + suffix_value["gloss"];	//$analysis{"gloss"} = $$prefix_value{"gloss"}."+".$$stem_value{"gloss"}."+".$$suffix_value{"gloss"};
 
-                                analysis.prob = current_prob; 
-
-				    			analysis.lex 	= toUnicode(analysis.lex)														//var %analysis = %{ &featureMergePSS($prefix_value,$stem_value,$suffix_value) };
-
-				   				analysis.lexgloss = stem_value["gloss"]																//# $analysis{"gloss"} = $$stem_value{"gloss"};
-
-							    analysis.gloss 	= prefix_value["gloss"] + " "+ stem_value["gloss"] + " " + suffix_value["gloss"];	//$analysis{"gloss"} = $$prefix_value{"gloss"}."+".$$stem_value{"gloss"}."+".$$suffix_value{"gloss"};
-
-                                console.log("unvoc_str:", unvoc_str)
-
+                                /*console.log("unvoc_str:", unvoc_str)
                                 console.log("analysis:",analysis)
-
                                 console.log("prefix gloss:", prefix_value["gloss"])
                                 console.log("stem gloss:", stem_value["gloss"])
-
-                                console.log("suffix gloss:", suffix_value["gloss"])
+                                console.log("suffix gloss:", suffix_value["gloss"])*/
 
 						
                                 //Check if current probablity is greater than the max_segmentation
 
-                                if(current_prob>max_prob){
-                                    max_prob = current_prob;
-
-                                    output = analysis;
+                                if(current_prob > max_prob){
+                                    max_prob    = current_prob;
+                                    output      = analysis;
                                 }
 							}
 			    		}
@@ -191,78 +162,49 @@ function analyzeSolutions(word){
 
 	}
 
-    //#@output=@{ &trimMalformed(&markMalformed($hash,\@output)) };
-    //#@output=@{ &markMalformed($hash,\@output) };
-
-    //#Hard-coded solutions:
-    //#CLASS LATIN	pos:latin
-    //#CLASS DIGIT	pos:digit
-    //#CLASS PUNC	pos:punc
-    //#CLASS else	pos:noun_prop
-
-    //#if( @output == 0 ) { print STDERR "NO LEXICAL ANALYSES\n"; }
-
-
-    // Probably a more efficient way of doing the following step ?
-    //@output=@{ &unique(\@output) };
     console.log("selected:", output)
-    return output;//return \@output;
+    return output;                      //return \@output;
 
-
-}
-
-function BWMorphotactics(str){			//sub BWMorphotactics {
-    									//my ($str)=@_;
-
-    //str = str.replace(/^((wa|fa)?(bi|ka)?Al)\+([tvd\*rzs\$SDTZln])/, "$1$4~" )   $str =~ s/^((wa|fa)?(bi|ka)?Al)\+([tvd\*rzs\$SDTZln])/$1$4~/; # not moon letters
-    //$str =~ s/^((wa|fa)?lil)\+([tvd\*rzs\$SDTZln])/$1$3~/; # not moon letters
-    //$str =~ s/A\+a([pt])/A$1/; # e.g.: Al+HayA+ap
-    //$str =~ s/\{/A/g;
-    //$str =~ s/\+//g;
-
-    return str;							//return($str);
 
 }
 
 var segmentword = function(str){
 
-    var segmented = [];
-    var prefix_len = 0;
-    var suffix_len = 0;
-    var stem_len = 0;
-    var prefix="";
-    var suffix="";
-    var stem="";
-    var str_len = str.length;
+    var segmented   = [];
+    var prefix_len  = 0;
+    var suffix_len  = 0;
+    var stem_len    = 0;
+    var prefix      = "";
+    var suffix      = "";
+    var stem        = "";
+    var str_len     = str.length;
     var max_prefix_len = 7;
     var max_suffix_len = 9;
     //var max_prefix_len = hash->{"#MAXPRELEN#"};
     //var max_suffix_len = hash->{"#MAXSUFLEN#"};
 
     while ( prefix_len <= max_prefix_len ) {
-	prefix = str.substr(0, prefix_len);
-	if ( hash_prefix.hasOwnProperty(prefix)) {
-    // if (exists(hash["pre#$prefix"])) {
-	    stem_len = (str_len - prefix_len);
-	    suffix_len = 0;
-	    while ((stem_len >= 1) && (suffix_len <= max_suffix_len)) {
-		stem   = str.substr(prefix_len, stem_len);
-		suffix = str.substr((prefix_len + stem_len), suffix_len);
-        if (hash_suffix.hasOwnProperty(suffix)) {
-//		if (exists(hash["suf#$suffix"])) {
-		    segmented.push([prefix , stem , suffix]);
-		}
-		stem_len--;
-		suffix_len++;
-	    }
-	};
-	prefix_len++;
+    	prefix = str.substr(0, prefix_len);
+    	if ( hash_prefix.hasOwnProperty(prefix)) {                          // if (exists(hash["pre#$prefix"])) {
+    	    stem_len   = (str_len - prefix_len);
+    	    suffix_len = 0;
+    	    while ((stem_len >= 1) && (suffix_len <= max_suffix_len)) {
+        		stem   = str.substr(prefix_len, stem_len);
+        		suffix = str.substr((prefix_len + stem_len), suffix_len);
+                if (hash_suffix.hasOwnProperty(suffix)) {                      //	if (exists(hash["suf#$suffix"])) {
+        		    segmented.push([prefix , stem , suffix]);
+        		}
+        		stem_len--;
+        		suffix_len++;
+    	    }
+    	};
+    	prefix_len++;
     };
     return segmented;
 };
 
 function replaceAll(find, replace, str) {
-  return str.replace(new RegExp(find, 'g'), replace);
+    return str.replace(new RegExp(find, 'g'), replace);
 }
 
 function toUnicode(text){
